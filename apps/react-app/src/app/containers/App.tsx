4603b9b371213d8707e7dafe4048a550fc6ed129
rollinstar <junsh.rock@gmail.com>
2023-02-18 12:44:14 +0900

    Set a development environment and build a project
    * Configure a mono repo project
    * Initialize a react app
    * Add a global style using scss
    * Add a common button ui for react

---
+++ apps/react-app/src/app/containers/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {Button} from 'react-ui'
+
+const AppPage = styled.div`
+    font-size: 15px;
+    color: 1a202c;
+`
+
+export const App = () => {
+    return (
+        <AppPage>
+            <h1>Ptolemaeus</h1>
+            <Button variant='outlined' onClick={() => console.log('Button')}>
+                상세보기
+            </Button>
+        </AppPage>
+    );
+}
